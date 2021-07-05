00947c32bae5c9ed2ae3037c26a954aaa059a505
HmThanina <86874243+HmThanina@users.noreply.github.com>
2021-07-05 19:45:33 +0100

    Update Symbole.java

--- src/TPinterpreteur/Symbole.java
+++ src/TPinterpreteur/Symbole.java
@@ -1,9 +1,32 @@
 package TPinterpreteur;
-
-public abstract class Symbole {
+public class Symbole{
     private String nom;
-
-    public Symbole(String nom) {
-        this.nom = nom;
+    //private int value;
+    Symbole(String nom, int value)
+    {
+        this.nom=nom;
+        //this.value=value;
     }
+    Symbole(){}
+    public int hashCode()
+    {
+        return nom.hashCode();
+    }
+    public boolean equals(Symbole symbole)
+    {
+        return nom.equals(symbole.getNom());
+    }
+    public String getNom()
+    {
+        return nom;
+    }
+    /* public int getValue()
+    {
+        return value;
+    }*/
+    public String toString()
+    {
+        return nom;
+    }
+    
 }
