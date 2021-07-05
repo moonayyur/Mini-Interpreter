3add37be6ba249b58e2e89155984d94d1e517f12
moonayyur <jm_kias@esi.dz>
2021-07-05 18:04:12 +0100

    expression,terme,facteur,element en cours

--- src/TPinterpreteur/Fonction.java
+++ src/TPinterpreteur/Fonction.java
@@ -1,6 +1,15 @@
 package TPinterpreteur;
 
 public class Fonction extends Symbole implements Evaluation {
+    private double resultat;
+    private Fonction_Standard type;
+    private Evaluation contenu;
+    public Fonction(String nom,double resultat,Fonction_Standard type,Evaluation contenu) {
+        super(nom);
+        this.resultat = resultat;
+        this.type = type;
+        this.contenu = contenu;
+    }
     @Override
     public double evaluer() {
         return 0;
