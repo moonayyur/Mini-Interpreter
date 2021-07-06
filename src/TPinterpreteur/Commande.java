681bfe4a544f0a3678da3bff5e8781fc30c01358
HmThanina <86874243+HmThanina@users.noreply.github.com>
2021-07-06 22:34:17 +0100

    Update Commande.java

--- src/TPinterpreteur/Commande.java
+++ src/TPinterpreteur/Commande.java
@@ -1,4 +1,13 @@
 package TPinterpreteur;
 
-public abstract class Commande {
+public abstract class Commande 
+  protected String expression;// l'expression a traiter
+    Commande(String expression)
+    {
+        this.expression=expression;
+    }
+    Commande(){}
+  
+    abstract public void affichageResultat();// affiche le resultat
+    
 }
