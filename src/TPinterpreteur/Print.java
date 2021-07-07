3b6da6eb4c6f2dc013bb59ea8988e4e1fa4c90a8
HmThanina <86874243+HmThanina@users.noreply.github.com>
2021-07-07 20:54:53 +0100

    Update Print.java

--- src/TPinterpreteur/Print.java
+++ src/TPinterpreteur/Print.java
@@ -1,4 +1,17 @@
 package TPinterpreteur;
 
 public class Print extends Commande{
+    private Expression expression;
+    Print(String expression)
+    {
+        super(expression);
+    }
+    public void affichageResultat()
+    {
+        
+
+    }
+   
+    
+    
 }
